81b91f67fb57a9be6996b2cd5489904eaeac511a
Krypt0niT <k269692@gmail.com>
2022-12-15 20:25:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,37 +7,57 @@ let playerColor = "rgba(0, 255, 0, 1)";
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(30, 30, playerColor, 10, 120);
+    myGamePiece = new component(30, 30, "red", 10, 120);
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = ScreenX;
-        this.canvas.height = ScreenY;
+        this.canvas.height =  ScreenY = 960;
+        ;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    }, 
+    clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
 function component(width, height, color, x, y) {
+    this.gamearea = myGameArea;
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
     this.x = x;
     this.y = y;    
-    this.update = function(){
+    this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
     }
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.x += 1;
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;    
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();    
     myGamePiece.update();
 }
